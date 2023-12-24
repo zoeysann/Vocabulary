e643fd67ecc94f3198b9c7bae608ba8ac66cd7f6
zoey <120593448+zoeysann@users.noreply.github.com>
2023-12-24 04:30:13 +0400

    Add files via upload

---
+++ Vocabulary/version1.py
@@ -0,0 +1,39 @@
+import random
+import time
+
+print("Hallo!")
+time.sleep(0.5)
+
+vocabulary={
+    "sehr": "very",
+    "gut": "good",
+    "natürlich": "of course",
+    "koche": "cook",
+    "schach": "chess",
+    "manchmal": "sometimes",
+    "klein": "small",
+    "gross": "big",
+    "klug": "smart",
+    "nett": "nice",
+    "bitte": "please",
+    "tschüss": "bye"
+    }
+
+n=int(input("Choose the number of questions: "))
+tcount=0
+
+for i in range(n):
+    ran_word=random.choice(list(vocabulary.keys()))
+    true_answer=vocabulary[ran_word]
+    
+    time.sleep(1)
+    question="Translate it"
+    print(question, ran_word, sep=": ")
+    your_answer=input("Your answer: ")
+    print("True answer: ",true_answer)
+    
+    
+    if your_answer==true_answer:
+        tcount=tcount+1
+
+print("The number of true asnwers is: ", tcount)
